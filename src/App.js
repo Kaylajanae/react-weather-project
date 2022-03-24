5b23ca2a972d381cd63d4972a0180bb0ce1ba107
Kaylajanae <35204914+Kaylajanae@users.noreply.github.com>
2022-03-24 16:18:55 -0700

    Update App.js
    Added open source GitHub link

--- src/App.js
+++ src/App.js
@@ -2,5 +2,19 @@ import React from "react";
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Hello Weather</div>;
+  return (
+    <div className="App">
+      Weather App
+      <footer>
+        This project was coded by Kaylajanae Saenz and is{" "}
+        <a
+          href="https://github.com/Kaylajanae/react-weather-project"
+          target="_blank"
+          rel=""
+        >
+          open sourced on github
+        </a>
+      </footer>{" "}
+    </div>
+  );
 }
