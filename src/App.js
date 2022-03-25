8e7a74871e10bfbe47d8fec4419220519cc08c60
Kaylajanae <35204914+Kaylajanae@users.noreply.github.com>
2022-03-24 18:08:55 -0700

    added opensource and netifly links

--- src/App.js
+++ src/App.js
@@ -4,17 +4,25 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      Weather App
+      <h1>Weather App</h1>
       <footer>
         This project was coded by Kaylajanae Saenz and is{" "}
         <a
           href="https://github.com/Kaylajanae/react-weather-project"
           target="_blank"
-          rel=""
+          rel="noreferrer"
         >
-          open sourced on github
+          open-sourced on Github
+        </a>{" "}
+        and{" "}
+        <a
+          href="https://strong-gumdrop-5b81f1.netlify.app/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          hosted on Netlify
         </a>
-      </footer>{" "}
+      </footer>
     </div>
   );
 }
