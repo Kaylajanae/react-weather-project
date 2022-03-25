05bf4f1777942c0b593a8d3301cbdf3b52a83637
Kaylajanae <35204914+Kaylajanae@users.noreply.github.com>
2022-03-24 18:59:17 -0700

    Added bootstrap to project

--- src/App.js
+++ src/App.js
@@ -4,25 +4,27 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
-      <footer>
-        This project was coded by Kaylajanae Saenz and is{" "}
-        <a
-          href="https://github.com/Kaylajanae/react-weather-project"
-          target="_blank"
-          rel="noreferrer"
-        >
-          open-sourced on Github
-        </a>{" "}
-        and{" "}
-        <a
-          href="https://strong-gumdrop-5b81f1.netlify.app/"
-          target="_blank"
-          rel="noreferrer"
-        >
-          hosted on Netlify
-        </a>
-      </footer>
+      <div className="container">
+        <h1>Weather App</h1>
+        <footer>
+          This project was coded by Kaylajanae Saenz and is{" "}
+          <a
+            href="https://github.com/Kaylajanae/react-weather-project"
+            target="_blank"
+            rel="noreferrer"
+          >
+            open-sourced on Github
+          </a>{" "}
+          and{" "}
+          <a
+            href="https://strong-gumdrop-5b81f1.netlify.app/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            hosted on Netlify
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
